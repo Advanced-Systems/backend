53e463eeee63abdc67524cf2b054a0fbb41bc45b
StefanGreve <greve.stefan@outlook.jp>
2023-05-21 14:40:16 +0200

    Organize startup into various methods with Main as the entry point of the application

--- AdvancedSystems.Backend/Program.cs
+++ AdvancedSystems.Backend/Program.cs
@@ -1,25 +1,38 @@
-var builder = WebApplication.CreateBuilder(args);
+namespace AdvancedSystems.Backend;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        ConfigureWebBuilder(ref builder);
+
+        var app = builder.Build();
+        ConfigureWebApp(ref app);
+        app.Run();
+
+    }
+
+    private static void ConfigureWebBuilder(ref WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+    }
+
+    private static void ConfigureWebApp(ref WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(options => {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                options.RoutePrefix = string.Empty;
+            });
+        }
+
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
