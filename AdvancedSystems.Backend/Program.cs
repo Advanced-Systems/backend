8a435e0bb29354f6c9ae35d1be07ea2719570c27
StefanGreve <greve.stefan@outlook.jp>
2023-05-21 15:41:30 +0200

    Use dependency injection for configuration

--- AdvancedSystems.Backend/Program.cs
+++ AdvancedSystems.Backend/Program.cs
@@ -4,35 +4,15 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
-        ConfigureWebBuilder(ref builder);
-
-        var app = builder.Build();
-        ConfigureWebApp(ref app);
-        app.Run();
-
+        CreateHostBuilder(args).Build().Run();
     }
 
-    private static void ConfigureWebBuilder(ref WebApplicationBuilder builder)
+    private static IHostBuilder CreateHostBuilder(string[] args)
     {
-        builder.Services.AddControllers();
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-    }
-
-    private static void ConfigureWebApp(ref WebApplication app)
-    {
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI(options => {
-                options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-                options.RoutePrefix = string.Empty;
+        return Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(builder =>
+            {
+                builder.UseStartup<Startup>();
             });
-        }
-
-        app.UseHttpsRedirection();
-        app.UseAuthorization();
-        app.MapControllers();
     }
 }
